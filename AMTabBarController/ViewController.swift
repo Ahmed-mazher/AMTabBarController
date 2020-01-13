b8180368adf83e528269d43fc6c9c376e2ca7560
Ahmed Mazher <ahmedmazher17@gmail.com>
2020-01-13 09:45:32 +0200

    Initial Commit

---
+++ AMTabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AMTabBarController
+//
+//  Created by Rivile on 1/13/20.
+//  Copyright © 2020 Rivile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
